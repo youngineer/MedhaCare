19d8f6654d02014bfd23d0187a273ad5f7f9779a
youngineer <youngineer@protonmail.com>
2025-09-12 22:38:02 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Created App.css to include Tailwind CSS and DaisyUI theme configuration.
    - Implemented main App component with routing setup for AuthPage.
    - Developed AuthPage, Body, Footer, and Navbar components with basic structure.
    - Set up main entry point (main.tsx) for React application.
    - Created service files for authentication, patient, and therapist services.
    - Defined TypeScript interfaces and constants for future use.
    - Configured TypeScript settings with tsconfig files for app and node environments.
    - Established Vite configuration for React and Tailwind CSS integration.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import AuthPage from './components/AuthPage'
+import Body from './components/Body'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body />}>
+            <Route path='/auth' element={<AuthPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
