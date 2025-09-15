6103a81f12b8e12bb8772f4824194b3dd39f2715
youngineer <youngineer@protonmail.com>
2025-09-15 09:14:56 +0530

    feat: implement logout functionality in Navbar with alert handling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,37 @@
+import { useNavigate } from "react-router-dom"
+import authServices from "../services/authServices";
+import type { IAlert } from "../types/interfaces";
+import { useState } from "react";
+import AlertDialog from "./AlertDialog";
+
 
 
 const Navbar = () => {
+    const navigate = useNavigate();
+    const [alert, setAlert] = useState<IAlert | null>(null);
+    
 
+    async function handleLogout(): Promise<void> {
+        try {
+            const response = await authServices.logout();
+            setAlert({isError: false, message: response?.message});
+            setTimeout(() => {
+                setAlert(null);
+            }, 2000);
+            navigate("/auth");
+        } catch (error: any) {
+            setAlert({isError: true, message: error.message});
+            setTimeout(() => {
+                setAlert(null);
+            }, 2000);
+
+            console.error(error);
+        }
+    }
 
     return (
         <div>
-            <div className="navbar bg-neutral text-neutral-content shadow-sm">
+            <div className="fixed navbar bg-neutral text-neutral-content shadow-sm">
                 <div className="flex-1">
                     <a className="btn btn-ghost text-xl">mindCare</a>
                 </div>
@@ -21,13 +47,14 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Edit Profile</a></li>
-                        <li><a>Logout</a></li>
+                        <li onClick={() => navigate("")}><a>Your Profile</a></li>
+                        <li onClick={handleLogout}><a>Logout</a></li>
                     </ul>
                     </div>
                 </div>
-                </div>
-        {/* {alert && (<AlertDialog {...alert!}/>)} */}
+                {alert && (<AlertDialog {...alert} />)}
+            </div>
+        
         </div>
     )
 }
